85de5f202e95b9f188ae3c04849cf38e771be7f9
Bruno Moretti <brmoretti@gmail.com>
2025-08-13 23:25:13 -0300

    Implement initial version of format_ft_time.py to calculate and display time elapsed since epoch

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+dt_init = datetime(1970, 1, 1)
+dt_end = datetime.now()
+time_elapsed = (dt_end - dt_init).total_seconds()
+
+print(
+	f"Seconds since {datetime.strftime(dt_init, '%B %-d, %Y')}: "
+	f"{time_elapsed:,.4f} or {time_elapsed:.2e} in scientific notation"
+	"\n"
+	f"{datetime.strftime(dt_end, '%b %-d %Y')}"
+)
