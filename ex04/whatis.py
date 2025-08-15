94a92066da9db4fd72b756e349b2a30a81626fc3
Bruno Moretti <brmoretti@gmail.com>
2025-08-14 23:05:20 -0300

    Implement whatis.py to determine if a number is odd or even based on command-line input

---
+++ ex04/whatis.py
@@ -0,0 +1,16 @@
+from sys import argv, exit
+
+if len(argv) == 1:
+	exit(0)
+
+try:
+	if len(argv) > 2:
+		raise(AssertionError("more than one argument is provided"))
+	try:
+		number = int(argv[1])
+	except ValueError:
+		raise(AssertionError("argument is not an integer"))
+	print("I'm " + ("Odd" if number % 2 else "Even") + ".")
+except AssertionError as e:
+	print(f"AssertionError: {e}")
+	exit(1)
