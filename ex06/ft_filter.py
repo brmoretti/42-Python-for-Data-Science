4eccd0e4ef3fdbeede65b2f950a290d8d3009f55
Bruno Moretti <brmoretti@gmail.com>
2025-08-15 16:28:15 -0300

    Add ft_filter.py to implement a custom filter function for iterables

---
+++ ex06/ft_filter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def ft_filter(function, iterable):
+    """
+    ft_filter(function or None, iterable) --> filter object
+    Return an iterator yielding those items of iterable for which
+    function(item) is true. If function is None, return the items
+    that are true.
+    """
+    if (function is None):
+        return (i for i in iterable if i)
+    return (i for i in iterable if function(i))
