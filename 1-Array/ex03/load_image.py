e1a94ce812c67415e787c134461405d510a5ba17
Bruno Moretti <brmoretti@gmail.com>
2025-08-22 10:44:57 -0300

    Implement ft_load function to load images as NumPy arrays with error handling

---
+++ 1-Array/ex03/load_image.py
@@ -0,0 +1,31 @@
+from PIL import Image, UnidentifiedImageError
+import numpy as np
+
+
+def ft_load(path: str) -> np.ndarray:
+    """
+    Loads an image from the specified file path and returns it as a NumPy
+    array.
+
+    Parameters:
+        path (str): The file path to the image.
+
+    Returns:
+        np.ndarray: The image represented as a NumPy array. If loading fails,
+        returns an empty array.
+
+    Raises:
+        Prints an error message if the file is not found, the image cannot be
+        identified, the file is not a valid image, or the loaded array is
+        empty.
+    """
+    try:
+        with Image.open(path) as im:
+            arr = np.array(im)
+        assert arr.size > 0, f"image {path} array is empty"
+        print(f"The shape of image is: {arr.shape}")
+        return arr
+    except (FileNotFoundError, UnidentifiedImageError, ValueError,
+            PermissionError, AssertionError) as e:
+        print(f"{type(e).__name__}: {e}")
+    return np.array([])
