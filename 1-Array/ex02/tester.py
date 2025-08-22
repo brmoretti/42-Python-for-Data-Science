fce1f1523a8c4de516d011faa19cccb8e202a1b3
Bruno Moretti <brmoretti@gmail.com>
2025-08-22 08:10:47 -0300

    Add commented-out test cases for ft_load function in tester.py

--- 1-Array/ex02/tester.py
+++ 1-Array/ex02/tester.py
@@ -2,3 +2,16 @@ from load_image import ft_load
 
 
 print(ft_load("landscape.jpg"))
+# print(ft_load("animal.jpeg"))
+# print(ft_load(""))
+# print(ft_load("hello.txt"))
+
+# # No permission test:
+# import os
+
+# file_path = "landscape.jpg"
+# os.chmod(file_path, 0o200)
+
+# print(ft_load("landscape.jpg"))
+
+# os.chmod(file_path, 0o644)
